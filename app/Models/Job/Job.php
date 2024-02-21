792c2c602962cb9bbb86d9655830ee48f6a92c66
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-02-21 20:52:37 +0600

    model & migrations table_home page_b

---
+++ app/Models/Job/Job.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models\Job;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'job_title',
+        'job_region',
+        'job_type',
+        'vacancy',
+        'experience',
+        'salary',
+        'gender',
+        'application_deadline',
+        'jobdescription',
+        'responsibilities',
+        'education_experience',
+        'otherbenifits',
+        'image'
+
+    ];
+
+}
---
+++ database/migrations/2024_02_21_132915_create_jobs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('job_title');
+            $table->string('job_region');
+            $table->string('job_type');
+            $table->integer('vacancy');
+            $table->string('experience');
+            $table->decimal('salary', 10, 2);
+            $table->string('gender');
+            $table->date('application_deadline');
+            $table->text('jobdescription');
+            $table->text('responsibilities');
+            $table->text('education_experience');
+            $table->text('otherbenifits');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
