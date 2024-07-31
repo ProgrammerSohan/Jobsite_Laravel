343934b6bd7ea97c54ef7d5f357ad700312c7f4e
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-07-31 10:04:07 +0600

    validating & cleaning up p1b

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -56,7 +56,14 @@
               {{ Auth::guard('admin')->user()->email }}
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Logout</a>
+              <a class="dropdown-item" href="{{route('logout')}}"  onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                      
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+               </form>
 
           </li>
           @else
