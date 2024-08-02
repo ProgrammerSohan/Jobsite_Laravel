5e2c889f950cc0ae40acffe358b494562400a556
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-08-02 08:16:06 +0600

    showing all admins_b

--- resources/views/admins/all-admins.blade.php
+++ resources/views/admins/all-admins.blade.php
@@ -16,24 +16,15 @@
                   </tr>
                 </thead>
                 <tbody>
+
+                @foreach($admins as $admin)
                   <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                   
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                   
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                   
-                  </tr>
+                    <th scope="row">{{ $admin->id }}</th>
+                    <td>{{ $admin->name }}</td>
+                    <td>{{ $admin->email }}</td>
+                 </tr>
+                @endforeach
+
                 </tbody>
               </table> 
             </div>
